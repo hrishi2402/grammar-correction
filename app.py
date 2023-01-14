d2cba240d0de5538d1fab1811be53f1185ece0d2
Hrishikesh Paradkar <hrishi2423@gmail.com>
2023-01-14 14:43:00 +0530

    Everything Working

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
-from flask import Flask
+from flask import Flask,request,render_template,redirect
+from gramformer import Gramformer
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=['GET', 'POST'])
 def hello_world():
-    return "<p>Hello, World!</p>"
+    if request.method == "GET":
+        print(request)
+        return render_template('home.html')
+    elif request.method == "POST":
+        try:
+            text = request.form.get('text')        
+        except:
+            raise IOError()    
+        if text:   
+            output = remove_errors(text)
+            for i in output:
+                output = {"output":i,"original":text} 
+            return render_template('home.html',output=output)
+        else:
+            redirect('hello_world')
+
+def remove_errors(input):
+    gf = Gramformer(models=1, use_gpu=False)
+    a = gf.correct(input)
+    return a
